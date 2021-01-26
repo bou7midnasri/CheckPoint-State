8796253534f6752ce469a93c0999b4bb568235f1
Ahmed <bouhmidnasri88@gmail.com>
2021-01-26 17:26:33 +0100

    Check Point State

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import Person from './Person'
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
+  const [show, setShow] = useState(false);
+  const Visibilité=()=>{
+    setShow(!show);
+    console.log(show);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Profil Person</h1>
+      <button type="button" className="btn btn-lg btn-primary" onClick={Visibilité}>
+        {show ? 'Hide Person' : 'Show Person'}
+      </button>
+     { show && <Person/> }
+     
     </div>
   );
 }
